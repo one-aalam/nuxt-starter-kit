d163bfaec04b1354e0c6058dd13125be87be840d
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-10-24 19:43:16 +0530

    Format components

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -1,53 +1,56 @@
 <template>
-    <div class="gg-spinner" :class="props.size"></div>
+  <div class="gg-spinner" :class="props.size"></div>
 </template>
 <script lang="ts" setup>
-    type SpinnerSize = 'lg' | 'xl' | 'xxl'
+type SpinnerSize = 'lg' | 'xl' | 'xxl'
 
-    const props = withDefaults(defineProps<{ size?: SpinnerSize }>(), {
-        size: 'lg'
-    })
+const props = withDefaults(defineProps<{ size?: SpinnerSize }>(), {
+  size: 'lg',
+})
 </script>
 <style>
 .gg-spinner {
-    transform: scale(var(--ggs,1))
+  transform: scale(var(--ggs, 1));
 }
 .lg {
-    transform: scale(var(--ggs,2))
+  transform: scale(var(--ggs, 2));
 }
 .xl {
-    transform: scale(var(--ggs,3))
+  transform: scale(var(--ggs, 3));
 }
 .xxl {
-    transform: scale(var(--ggs,4))
+  transform: scale(var(--ggs, 4));
 }
 .gg-spinner,
 .gg-spinner::after,
 .gg-spinner::before {
-    box-sizing: border-box;
-    position: relative;
-    display: block;
-    width: 20px;
-    height: 20px
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  width: 20px;
+  height: 20px;
 }
 .gg-spinner::after,
 .gg-spinner::before {
-    content: "";
-    position: absolute;
-    border-radius: 100px
+  content: '';
+  position: absolute;
+  border-radius: 100px;
 }
 .gg-spinner::before {
-    animation: spinner 1s
-    cubic-bezier(.6,0,.4,1) infinite;
-    border: 3px solid transparent;
-    border-top-color: currentColor
+  animation: spinner 1s cubic-bezier(0.6, 0, 0.4, 1) infinite;
+  border: 3px solid transparent;
+  border-top-color: currentColor;
 }
 .gg-spinner::after {
-    border: 3px solid;
-    opacity: .2
+  border: 3px solid;
+  opacity: 0.2;
 }
 @keyframes spinner {
-    0% { transform: rotate(0deg) }
-    to { transform: rotate(359deg) }
+  0% {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
 }
 </style>
