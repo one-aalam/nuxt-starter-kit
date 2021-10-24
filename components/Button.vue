7866e718f71d8e84088d5e68e01055e4ee385f80
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-10-24 19:42:59 +0530

    Improve Button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,8 +1,47 @@
 <template>
-    <button class="button text-green-400">Sign In!</button>
+  <button
+    :type="type"
+    class="btn"
+    :class="[
+      `btn-${size} btn-${variant}`,
+      {
+        loading: loading,
+        'btn-wide': wide,
+        'btn-circle': circle,
+        'btn-outline': outline,
+      },
+    ]"
+    :disabled="disabled"
+    :="$attrs"
+  >
+    <slot />
+  </button>
 </template>
 
 <script lang="ts" setup>
-    import { ref } from 'vue'
-    const message = ref<string>('This is a message')
+type ButtonType = 'submit' | 'button' | 'reset'
+type ButtonVariant = 'primary' | 'secondary' | 'accent'
+type ButtonSize = 'xs' | 'sm' | 'md' | 'lg'
+
+type ButtonProps = {
+  type?: ButtonType
+  variant?: ButtonVariant
+  size?: ButtonSize
+  wide?: boolean
+  circle?: boolean
+  outline?: boolean
+  loading: boolean
+  disabled: boolean
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: 'button',
+  variant: 'primary',
+  size: 'md',
+  wide: false,
+  circle: false,
+  outline: false,
+  loading: false,
+  disabled: false,
+})
 </script>
