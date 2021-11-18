2ff8e683ac8e048704370c36eac45743af5bb581
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-11-18 20:13:43 +0530

    feat: Add protection for private/auth-only pages/routes

---
+++ components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="w-full py-3 bg-blue-50 shadow-md flex justify-center">
+            <ul class="list-none flex gap-4 text-center">
+                <!-- <li>
+                    <NuxtLink to="/">
+                        <IconHome class="inline-block w-7 h-7 text-blue-600"/>
+                        Home
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/profile">
+                        <IconUser class="w-8 h-8 inline-block text-blue-600" />
+                        Profile
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/settings">
+                    <IconCog class="w-8 h-8 inline-block text-blue-500" />
+                        Settings
+                    </NuxtLink>
+                </li> -->
+                <li>
+                    {{props.user && props.user.email}} <button v-if="props.user" @click="$emit('signOut')">(Sign Out)</button>
+                </li>
+            </ul>
+        </nav>
+</template>
+
+<script lang="ts" setup>
+    import type { User } from '@supabase/supabase-js'
+    import IconHome from '~icons/heroicons-solid/home'
+    import IconUser from '~icons/heroicons-solid/user'
+    import IconCog from '~icons/heroicons-solid/cog'
+
+    type NavProps = {
+        user: User
+    }
+
+    const props = defineProps<NavProps>()
+
+        console.log(props)
+
+    type NavEvts = {
+        (e: 'signOut'): void
+    }
+
+    const emit = defineEmits<NavEvts>()
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,22 @@
         <slot />
     </div>
 </template>
+<script lang="ts" setup>
+    import type { Subscription } from '@supabase/supabase-js'
+
+    const { $supabase } = useNuxtApp()
+    const subscription = ref<{ data: Subscription | null }>()
+
+    onMounted(() => {
+        subscription.value = $supabase.auth.onAuthStateChange(async(event, session) => {
+            await $fetch('/api/auth', {
+                method: 'POST',
+                body: { event, session }
+            })
+        })
+    })
+
+    onUnmounted(() => {
+        if(subscription.value) subscription.value?.data?.unsubscribe()
+    })
+</script>
