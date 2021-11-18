202c8e5bf3edd3d9247794a086758ccbb699ff27
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-11-18 20:38:40 +0530

    docs: Update Starter Kit description

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center">
       <StarterKit/>
-      <p class="text-center">Find Supabase integrated as a plugin, and full-stack authentication support alongwith components like alert/toast, button, separator, modal/dialog, spinner, etc. </p>
+      <p class="text-center">Nuxt3 with components like alert/toast, button, separator, modal/dialog, spinner, etc. For Supabase integration as a plugin, and full-stack authentication support checkout the <strong>main</strong> branch</p>
       <Sep/>
       <div class="text-center">
         <NuxtLink class="btn btn-primary btn-outline" to="/gallery">Browse Components</NuxtLink>&nbsp;
