b563cba816796b9b42c219eb728a7e5ceed0002f
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-11-18 19:34:43 +0530

    feat: Add a /gallery page for component examples

---
+++ components/ComponentExample.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-center">
+        <h3 v-if="heading" class="inline-block my-8 px-2 py-1 rounded-md uppercase bg-gray-600 text-white">{{props.heading}}</h3>
+        <div class="components text-center flex place-content-center gap-20 min-h-80">
+            <slot/>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+    const props = withDefaults(defineProps<{ heading?: string }>(), {
+      heading: '',
+    })
+</script>
